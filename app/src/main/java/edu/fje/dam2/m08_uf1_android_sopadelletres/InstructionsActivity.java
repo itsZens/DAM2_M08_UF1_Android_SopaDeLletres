fb10cbe41de2dc53a46c928e54d82fa4be738f5e
Daniel Muelle <danimr1999@gmail.com>
2019-12-17 20:38:13 +0100

    UI MainActivity (buttons working)

---
+++ app/src/main/java/edu/fje/dam2/m08_uf1_android_sopadelletres/InstructionsActivity.java
@@ -0,0 +1,14 @@
+package edu.fje.dam2.m08_uf1_android_sopadelletres;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InstructionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instructions);
+    }
+}
--- app/src/main/java/edu/fje/dam2/m08_uf1_android_sopadelletres/MainActivity.java
+++ app/src/main/java/edu/fje/dam2/m08_uf1_android_sopadelletres/MainActivity.java
@@ -1,14 +1,59 @@
 package edu.fje.dam2.m08_uf1_android_sopadelletres;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button playButton;
+    private Button scoresButton;
+    private Button instructionsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Button "Jugar"
+        playButton = findViewById(R.id.playButton);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+
+                // Start a new activity onClick the button
+                Intent intent = new Intent(MainActivity.this, PlayActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Button "Puntuacions"
+        scoresButton = findViewById(R.id.scoresButton);
+        scoresButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+
+                // Start a new activity onClick the button
+                Intent intent = new Intent(MainActivity.this, ScoreboardsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Button "Instructions"
+        instructionsButton = findViewById(R.id.instructionsButton);
+        instructionsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+
+                // Start a new activity onClick the button
+                Intent intent = new Intent(MainActivity.this, InstructionsActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
 }
