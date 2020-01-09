f46ad91f9768e34ec8436003ed69239bb0d3e0e5
Daniel Muelle <danimr1999@gmail.com>
2020-01-09 15:31:21 +0100

    Button backToMainMenu working on AfterGameScoreActivity

--- app/src/main/java/edu/fje/dam2/m08_uf1_android_sopadelletres/AfterGameScoreActivity.java
+++ app/src/main/java/edu/fje/dam2/m08_uf1_android_sopadelletres/AfterGameScoreActivity.java
@@ -1,14 +1,31 @@
 package edu.fje.dam2.m08_uf1_android_sopadelletres;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 public class AfterGameScoreActivity extends AppCompatActivity {
+
+    private Button backToMainMenu;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_after_game_score);
+
+        // Button "Menú principal"
+        backToMainMenu = findViewById(R.id.playButton);
+        backToMainMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+
+                // Start a new activity onClick the button
+                Intent intent = new Intent(AfterGameScoreActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
