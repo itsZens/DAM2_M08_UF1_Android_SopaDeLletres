729c80a5292f5766e779bcdfdd1a402eb352056b
Daniel Muelle <danimr1999@gmail.com>
2020-01-11 11:35:13 +0100

    Fixed SQLite creation table on MainActivity Added to Git Instructions.html

--- app/src/main/java/edu/fje/dam2/m08_uf1_android_sopadelletres/MainActivity.java
+++ app/src/main/java/edu/fje/dam2/m08_uf1_android_sopadelletres/MainActivity.java
@@ -66,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
             // Creating the table "SopaDeLletres"
             database = this.openOrCreateDatabase("SopaDeLletres", MODE_PRIVATE, null);
 
-            database.execSQL("CREATE TABLE IF NOT EXISTS Scoreboards(username VARCHAR, scduration TEXT, date TEXT);");
+            database.execSQL("CREATE TABLE IF NOT EXISTS Scoreboards(username VARCHAR, score INT, game_duration TEXT, date TEXT);");
 
             Log.i("MainActivity","Database created successfully");
 
--- app/src/main/java/edu/fje/dam2/m08_uf1_android_sopadelletres/ScoreboardsActivity.java
+++ app/src/main/java/edu/fje/dam2/m08_uf1_android_sopadelletres/ScoreboardsActivity.java
@@ -2,7 +2,9 @@ package edu.fje.dam2.m08_uf1_android_sopadelletres;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 
 public class ScoreboardsActivity extends AppCompatActivity {
 
@@ -10,5 +12,25 @@ public class ScoreboardsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scoreboards);
+    }
+
+    protected void saveScoreToDatabase(){
+        // Database SQLite connection
+        SQLiteDatabase database = null;
+
+        try {
+            // Creating the table "SopaDeLletres"
+            database = this.openOrCreateDatabase("SopaDeLletres", MODE_PRIVATE, null);
+
+            database.execSQL("INSERT INTO SopaDeLletres() ;");
+
+            Log.i("MainActivity","Database created successfully");
+
+
+        } finally {
+            if (database != null) {
+                database.close();
+            }
+        }
     }
 }
